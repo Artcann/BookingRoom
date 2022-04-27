6f97b0ea04690eeba099b2aff82ce5d5315025d4
Artcann <art.cann@orange.fr>
2022-04-27 18:44:48 +0200

    Update primary key generation for postgres

--- src/main/java/fr/isep/bookingRoom/domain/Userdata.java
+++ src/main/java/fr/isep/bookingRoom/domain/Userdata.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 @AllArgsConstructor
 public class Userdata {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String username;
